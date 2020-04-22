ee544fe624686ca1cffcb2fe6a3f30bb6cd50549
Chen <chen.zhang3@wework.com>
2020-04-22 17:39:20 -0400

    remove koin

--- app/src/main/java/com/mmlodawski/jvmoverloadsmediumexample/activity/MainActivity.kt
+++ app/src/main/java/com/mmlodawski/jvmoverloadsmediumexample/activity/MainActivity.kt
@@ -1,15 +1,12 @@
 package com.mmlodawski.jvmoverloadsmediumexample.activity
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.mmlodawski.jvmoverloadsmediumexample.R
-import com.mmlodawski.jvmoverloadsmediumexample.toolbox.Navigator
 import kotlinx.android.synthetic.main.activity_main.*
-import org.koin.android.ext.android.inject
 
 class MainActivity : AppCompatActivity() {
-
-    private val navigator: Navigator by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,8 +15,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setUpListeners() {
-        buttons.setOnClickListener { navigator.navigateToButtons(this) }
-        textInputEditTexts.setOnClickListener { navigator.navigateToTextInputEditTexts(this) }
-        checkBoxes.setOnClickListener { navigator.navigateToCheckBoxes(this) }
+        buttons.setOnClickListener { startActivity(Intent(this@MainActivity, ButtonsActivity::class.java)) }
+        textInputEditTexts.setOnClickListener { startActivity(Intent(this@MainActivity, ButtonsActivity::class.java)) }
+        checkBoxes.setOnClickListener { startActivity(Intent(this@MainActivity, ButtonsActivity::class.java)) }
     }
 }
